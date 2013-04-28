a34a9e7949bb8995572c37493ddd2e123a676771
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2013-04-28 17:39:29 +0300

    feat: add FirefoxAurora and FirefoxNightly

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ var FirefoxBrowser = function(id, baseBrowserDecorator, logger) {
   };
 };
 
+
 FirefoxBrowser.prototype = {
   name: 'Firefox',
 
@@ -51,7 +52,44 @@ FirefoxBrowser.prototype = {
 FirefoxBrowser.$inject = ['id', 'baseBrowserDecorator', 'logger'];
 
 
+var FirefoxAuroraBrowser = function(id, baseBrowserDecorator, logger) {
+  FirefoxBrowser.call(this, id, baseBrowserDecorator, logger);
+}
+
+FirefoxAuroraBrowser.prototype = {
+  name: 'FirefoxAurora',
+  DEFAULT_CMD: {
+    linux: 'firefox',
+    darwin: '/Applications/FirefoxAurora.app/Contents/MacOS/firefox-bin',
+    win32: process.env.ProgramFiles + '\\Aurora\\firefox.exe'
+  },
+  ENV_CMD: 'FIREFOX_AURORA_BIN'
+};
+
+FirefoxAuroraBrowser.$inject = ['id', 'baseBrowserDecorator', 'logger'];
+
+
+var FirefoxNightlyBrowser = function(id, baseBrowserDecorator, logger) {
+  FirefoxBrowser.call(this, id, baseBrowserDecorator, logger);
+}
+
+FirefoxNightlyBrowser.prototype = {
+  name: 'FirefoxNightly',
+
+  DEFAULT_CMD: {
+    linux: 'firefox',
+    darwin: '/Applications/FirefoxNightly.app/Contents/MacOS/firefox-bin',
+    win32: process.env.ProgramFiles + '\\Nightly\\firefox.exe'
+  },
+  ENV_CMD: 'FIREFOX_NIGHTLY_BIN'
+};
+
+FirefoxNightlyBrowser.$inject = ['id', 'baseBrowserDecorator', 'logger'];
+
+
 // PUBLISH DI MODULE
 module.exports = {
-  'launcher:Firefox': ['type', FirefoxBrowser]
+  'launcher:Firefox': ['type', FirefoxBrowser],
+  'launcher:FirefoxAurora': ['type', FirefoxAuroraBrowser],
+  'launcher:FirefoxNightly': ['type', FirefoxNightlyBrowser]
 };
