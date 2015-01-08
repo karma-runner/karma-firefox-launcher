fcc02af29846e0fd5c7e7ac9a09aabbf8afb8a5f
Peter Johanson <peter@peterjohanson.com>
2015-01-08 01:35:21 -0500

    FreeBSD support OOTB.

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ FirefoxBrowser.prototype = {
 
   DEFAULT_CMD: {
     linux: 'firefox',
+    freebsd: 'firefox',
     darwin: '/Applications/Firefox.app/Contents/MacOS/firefox-bin',
     win32: getFirefoxExe('Mozilla Firefox')
   },
